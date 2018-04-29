e7bba4080224c509b50fa7ea69a77d2b3668af07
JerryDai90 <jerrydai90@gmail.com>
2018-04-30 01:28:58 +0800

    增加动态 json 数据
    1. 支持传入 json 数据来构建。
    2. 增加遮罩层来拖拉。
    3. 支持嵌入的时候有一个父级的 div 包裹。

--- js/draggrid-1.0.1.js
+++ js/draggrid-1.0.1.js
@@ -1,201 +1,261 @@
-$(function () {
+//$(function () {
 
-    /**
-                                                                                
-    1. 拖拉
-        1. 计算24个格子（x/y）的绝对像素点区间，比如
-            3.33333% = 0~20
-            10.33333% = 20~40
+    $.fn.draggrid = function(defOpt) {
+        return draggrid($(this), defOpt);
+    }
 
-        2. 修改大小的时候判断拖拉的长度，对比百分值的区间是否修改大小（显示出拖动的效果图）
+    var draggrid = function (containerObj, option) {
+//        var containerId = "container";
 
-        3. 拖动的时候需要安装原来大小进行拖动，也只能进行格子之间的移动。
+        var self = this;
+        var defOpt = {
+            /*切割的格子个数*/
+            gridNumber: 24,
+            /*触发移动元素的名称*/
+            btnMoveName: "_btnMove_",
+            /*触发改变大小的元素名称*/
+            btnResizeName: "_resize_",
 
-        4. 修改大小或者拖动的时候不能超出父容器的大小（24 * 24）
-
-    2. 嵌套
-        拖拉模板文件到指定的容器即可完成
-
-
-    3. 容器嵌入容器为什么里面容器的100%就是外面容器的100%
-    */
-
-    window.cache = {
-        currentDivObj: null //当前缓存的面板 div 对象
-    };
-
-    /*
-     * 和面板相关的方法
-     */
-    var panel = {
-        /*
-         * 获取当前选中的面板 div 的父容器信息，每个面板 div 务必携带属性 data-ispanel
-         */
-        getCurrentSuperiorDivObj: function () {
-            var temp = {
-                divObj: $(window),
-                isWindow: true
-            }
-            if (cache.currentDivObj != null) {
-                var ispanel = cache.currentDivObj.parent("[data-ispanel]");
-                if (ispanel.length != 0) {
-                    temp.divObj = ispanel;
-                    temp.isWindow = false;
-                }
-            }
-            return temp;
-        },
-        getContainerDiv: function (_this) {
-            return $(_this).closest('div.moveBar');
-        },
-        setOrResetZIndex: function (obj, isResetAction) {
-            var zindex = 0;
-            if (isResetAction) {
-                zindex = $(obj).attr("data-oldzindex") || 0;
-            } else {
-                $(obj).attr("data-oldzindex", $(obj).css("z-index"));
-                zindex = 99999;
-            }
-            $(obj).css("z-index", zindex);
-        },
-        /*
-         * 获取父容器无效的偏移量，具体指定父容器和最外层的坐标
-         */
-        getParentContainerInvalidOffset: function () {
-            //获取先对符框的顶点坐标，用于移动的时候减去外面的偏移量
-            var left = 0;
-            var top = 0;
-            var currentSuperiorDivObj = this.getCurrentSuperiorDivObj();
-            if (!currentSuperiorDivObj.isWindow) {
-                left = currentSuperiorDivObj.divObj.offset().left;
-                top = currentSuperiorDivObj.divObj.offset().top;
-            }
-            return {
-                left: left,
-                top: top
-            };
         }
 
-    };
+        $.extend(true, defOpt, option);
+        self.option = defOpt;
+
+        self._cache = {
+            /*当前选中的面板 div 对象*/
+            currentDivObj: null,
+            /*容器格子比例，通过 gridNumber 个数来算出每个格子的百分比*/
+            scaleList: [],
+        }
+
+        var util = {};
+        util.scaling = {
+            /*
+             * 通过移动的距离来计算接近的固定比例值
+             * @location 先对于父容器位移长度（也就是移动的高度或者宽度，只算移动距离）
+             * @parentContainerLength 父容器的长度（直属父容器的高度或者宽度）
+             */
+            transform: function (shiftLength, parentContainerLength, scaleList) {
+                var _this = this;
+                var xs = ((shiftLength - 2) / parentContainerLength * 100);
+                var temp = null;
+                $.each(_this.scaleList, function (i, _d) {
+                    if (_d > xs && _d <= _this.scaleList[i + 1]) {
+                        temp = _d;
+                        return false;
+                    }
+                });
+                return temp;
+            },
+            transformX: function (location) {
+                return this.transform(location, this.getTransformDivObj().width(), self._cache.scaleList) + "%";
+            },
+            transformY: function (location) {
+                return this.transform(location, this.getTransformDivObj().height(), self._cache.scaleList) + "%";
+            },
+            getTransformDivObj: function () {
+                return util.panel.getCurrentSuperiorDivObj().divObj;
+            },
+            initScaleList: function (gridNumber) {
+                gridNumber = undefined == gridNumber || null == gridNumber ? 24 : gridNumber;
+                var temp = [];
+                temp.push(0);
+                for (var i = 1; i <= gridNumber; i++) {
+                    temp.push(100 / gridNumber * i);
+                }
+                temp.push(200);
+                this.scaleList = temp;
+                console.log(temp);
+            }
+        }
 
 
-    /*
-     * 计算转率
-     */
-    var scaling = {
-        /*
-         * 24格比例，前后2个值为占位
-         */
-        scaleList: [0, 4.1666666667, 8.3333333334, 12.500000000, 16.666666667, 20.833333334, 25.00000000, 29.166666667, 33.333333334, 37.50000000, 41.666666667, 45.833333334, 50.000000000,
-                                54.166666667, 58.333333334, 62.500000001, 66.666666667, 70.833333334, 75.000000001, 79.166666667, 83.333333334, 87.500000001, 91.666666667, 95.833333334, 100.000000000, 200.000000000],
-        /*
-         * 通过移动的距离来计算接近的固定比例值
-         * @location 先对于父容器位移长度（也就是移动的高度或者宽度，只算移动距离）
-         * @parentContainerLength 父容器的长度（直属父容器的高度或者宽度）
-         */
-        transform: function (shiftLength, parentContainerLength) {
-            var _this = this;
-            var xs = ((shiftLength - 2) / parentContainerLength * 100);
-            var temp = null;
-            $.each(_this.scaleList, function (i, _d) {
-                if (_d > xs && _d <= _this.scaleList[i + 1]) {
-                    temp = _d;
-                    return false;
+        util.panel = {
+            /*
+             * 获取当前选中的面板 div 的父容器信息，每个面板 div 务必携带属性 data-ispanel
+             */
+            getCurrentSuperiorDivObj: function () {
+                debugger;
+                var temp = {
+                    divObj: $(window),
+                    isWindow: true
+                }
+                if (self._cache.currentDivObj != null) {
+                    var ispanel = self._cache.currentDivObj.parents("div[data-ispanel]");
+                    if (ispanel.length != 0) {
+                        temp.divObj = ispanel;
+                        temp.isWindow = false;
+                    }
+                }
+                return temp;
+            },
+            getContainerDiv: function (_this) {
+                return $(_this).closest('div.moveBar');
+            },
+            setOrResetZIndex: function (obj, isResetAction) {
+                var zindex = 0;
+                if (isResetAction) {
+                    zindex = $(obj).attr("data-oldzindex") || 0;
+                } else {
+                    $(obj).attr("data-oldzindex", $(obj).css("z-index"));
+                    zindex = 99999;
+                }
+                $(obj).css("z-index", zindex);
+            },
+            /*
+             * 获取父容器无效的偏移量，具体指定父容器和最外层的坐标
+             */
+            getParentContainerInvalidOffset: function () {
+                //获取先对符框的顶点坐标，用于移动的时候减去外面的偏移量
+                var left = 0;
+                var top = 0;
+                debugger;
+                var currentSuperiorDivObj = this.getCurrentSuperiorDivObj();
+                if (!currentSuperiorDivObj.isWindow) {
+                    left = currentSuperiorDivObj.divObj.offset().left;
+                    top = currentSuperiorDivObj.divObj.offset().top;
+                }
+                return {
+                    left: left,
+                    top: top
+                };
+            }
+        };
+
+
+        var event = function () {
+            $(document).mouseup(function (event) {
+
+                $(this).unbind("mousemove");
+                if (null != self._cache.currentDivObj) {
+                    util.panel.setOrResetZIndex(self._cache.currentDivObj, true);
+                    self._cache.currentDivObj.removeClass("opacity").children("[name=childShade]").hide();
+                    self._cache.currentDivObj = null;
                 }
             });
-            return temp;
-        },
-        transformX: function (location) {
-            return this.transform(location, this.getTransformDivObj().width()) + "%";
-        },
-        transformY: function (location) {
-            return this.transform(location, this.getTransformDivObj().height()) + "%";
-        },
-        getTransformDivObj: function () {
-            return panel.getCurrentSuperiorDivObj().divObj;
-        },
-        initScaleList : function(){
-            var temp = [];
-            temp.push(0);
-            for( var i = 1; i <= 84; i++ ){
-                temp.push(100/84*i);
-            }
-            temp.push(200);
-            this.scaleList = temp;
-            console.log(temp)
-        }
-    };
-    
-    scaling.initScaleList();
 
+            //改变位置
+            $(document).on("mousedown", "[name=" + self.option.btnMoveName + "]", function (event) {
+                var div = self._cache.currentDivObj = util.panel.getContainerDiv(this);
+                util.panel.setOrResetZIndex(div, false);
+                self._cache.currentDivObj.addClass("opacity");
 
+                //获取当前面板的坐标
+                var abs_x = event.pageX - div.offset().left;
+                var abs_y = event.pageY - div.offset().top;
+                //                console.log(".........>>>>>>>>>>>>>>..."+abs_x+" >"+abs_y);
 
-    $(document).mouseup(function (event) {
-        
-//            opacity: 0.7;
-        $(this).unbind("mousemove");
-        if (null != cache.currentDivObj) {
-            panel.setOrResetZIndex(cache.currentDivObj, true);
-            cache.currentDivObj.removeClass("opacity");
+                //获取先对符框的顶点坐标，用于移动的时候减去外面的偏移量
+                var parentContainerInvalidOffset = util.panel.getParentContainerInvalidOffset();
+                console.log(parentContainerInvalidOffset)
+
+                $(document).mousemove(function (event2) {
+                    div.css({
+                        'left': util.scaling.transformX(event2.pageX - parentContainerInvalidOffset.left - abs_x),
+                        'top': util.scaling.transformY(event2.pageY - parentContainerInvalidOffset.top - abs_y),
+                    });
+                });
+            });
+
+            //改变大小
+            $(document).on("mousedown", "[name=" + self.option.btnResizeName + "]", function (event) {
+                event.stopPropagation();
+                //面板对象    
+                var div = self._cache.currentDivObj = util.panel.getContainerDiv(this);
+                util.panel.setOrResetZIndex(div, false);
+
+                //获取当前此面板的 xy 定点坐标，用于算出面板宽度
+                var left = div.offset().left;
+                var top = div.offset().top;
+
+                $(document).mousemove(function (event2) {
+                    //得到面板的高宽
+                    var width = (event2.pageX - left);
+                    var height = (event2.pageY - top);
+                    div.css({
+                        "width": util.scaling.transformX(width),
+                        "height": util.scaling.transformY(height)
+                    });
+                })
+            });
             
-            cache.currentDivObj = null;
+            /*触发修改按钮*/
+            $(document).on("click", "[name=_btnTriggerResize_]", function (event) {
+                var div = self._cache.currentDivObj = util.panel.getContainerDiv(this);
+                div.children("[name=childShade]").show();
+            });
+            
+            /*删除*/
+            $(document).on("click", "[name=_btnDel_]", function (event) {
+                util.panel.getContainerDiv(this).remove();
+            });
+            
+            
         }
-    });
-
-    //改变位置
-    $('[name=banner]').mousedown(function (event) {
-        var div = cache.currentDivObj = panel.getContainerDiv(this);
-        panel.setOrResetZIndex(div, false);
-        cache.currentDivObj.addClass("opacity");
-
-        //获取当前面板的坐标
-        var abs_x = event.pageX - div.offset().left;
-        var abs_y = event.pageY - div.offset().top;
-        //                console.log(".........>>>>>>>>>>>>>>..."+abs_x+" >"+abs_y);
-
-        //获取先对符框的顶点坐标，用于移动的时候减去外面的偏移量
-        var parentContainerInvalidOffset = panel.getParentContainerInvalidOffset();
-
-        $(document).mousemove(function (event2) {
-
-            //                    if( panel.getCurrentSuperiorDivObj().divObj.width() <= ((event2.pageX - parentContainerInvalidOffset.left - abs_x) + parseFloat(div.css("width")))){
-            //                        return;  
-            //                    }
-            //                    if( panel.getCurrentSuperiorDivObj().divObj.height() <= ((event2.pageY - parentContainerInvalidOffset.top - abs_y) + parseFloat(div.css("height")))){
-            //                        return;  
-            //                    }
-            //                    
-
-            //                    console.log(panel.getCurrentSuperiorDivObj().divObj.width()+" -- " + )
-
-
-            div.css({
-                'left': scaling.transformX(event2.pageX - parentContainerInvalidOffset.left - abs_x),
-                'top': scaling.transformY(event2.pageY - parentContainerInvalidOffset.top - abs_y),
+        
+        //<div name="childPanel" style="width: 100%;height: 100%;"></div>\
+        /*
+        <span class="draggrid-hide"></span>\
+                            <span class="draggrid-hide"></span>\
+                            <span class="draggrid-hide"></span>\*/
+        var html = '\
+                    <div class="moveBar" name="moveBar" data-ispanel id="$id" data-pid="$pid" style="background: $background; top: $top%; left:$left%; width:$width%; height:$height%;">\
+                        <div name="banner" class="banner">\
+                            <a name="_btnDel_">删除</a>\
+                            <a name="_btnMove_">移动</a>\
+                            <a name="_btnTriggerResize_">修改大小</a>\
+                        </div>\
+                        <div name="childShade" style="width:100%; height: 100%; display: none; position: absolute; z-index: 10; background: white; opacity: 0.5;">\
+                            <img class="draggrid-resize-img" name="_resize_"></img>\
+                        </div>\
+                        <div name="childPanel" style="width:100%; height: 100%; position: absolute;"></div>\
+                    </div>';
+         var x = [
+            '#2E2EFE', '#FF00BF', '#FACC2E', '#FAAC58', '#61210B', '#08088A', '#E6E6E6'
+        ];
+        var template = function(i, _d){
+            var temp = html.replace(/\$id/g, _d.id).replace(/\$name/g, _d.name)
+                            .replace(/\$top/g, _d.y).replace(/\$left/g, _d.x)
+                            .replace(/\$width/g, _d.width).replace(/\$height/g, _d.height)
+                            .replace(/\$pid/g, _d.pId)
+                            .replace(/\$background/g, x[i]);
+            return temp;
+        }
+        
+        self.addGrid = function(jsonData){
+            $.each(jsonData, function(i, _d){
+                if( _d.pId == '-1' ){
+                    $(containerObj).append(template(i, _d));
+                }
             });
-        });
-    });
-
-    //改变大小
-    $('[name="resize"]').mousedown(function (event) {
-        //面板对象    
-        var div = cache.currentDivObj = panel.getContainerDiv(this);
-        panel.setOrResetZIndex(div, false);
-
-        //获取当前此面板的 xy 定点坐标，用于算出面板宽度
-        var left = div.offset().left;
-        var top = div.offset().top;
-
-        $(document).mousemove(function (event2) {
-            //得到面板的高宽
-            var width = (event2.pageX - left);
-            var height = (event2.pageY - top);
-            div.css({
-                "width": scaling.transformX(width),
-                "height": scaling.transformY(height)
+            
+            //子模块要算出先后顺序，否则会找不到父容器。
+            $.each(jsonData, function(i, _d){
+                if( _d.pId != '-1' ){
+                    $("#"+_d.pId).children("[name=childPanel]").html(template(i, _d));
+                }
             });
-        })
-    });
+        }
+        
+        self.on = {
+            addGridCallback : function(){}
+            
+        }
 
 
-});
+
+        var init = function () {
+            util.scaling.initScaleList(self.option.gridNumber);
+            event();
+        }
+
+        init();
+        containerObj.data("obj", self);
+        return self;
+    }
+
+//    draggrid({
+//        gridNumber: 48
+//    });
+
+//});
